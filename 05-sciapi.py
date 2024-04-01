00c8f30ba6a06675aa8b1d31e54ebc5f23b1e0eb
evillalbaa <evillalba001@gmail.com>
2024-04-01 17:51:58 +0200

    Update APIs and add ipiwitges

---
+++ 05-sciapi.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def alive():
+	'This resturns wheter the api is alive'
+	return({"status":"alive"})
+
+@app.get("/power/{n}")	
+def power(n: int ):
+	'This function computes the power 2 of n'
+	return({"nummber":n, "power":n**2})
+	
+class Operand(BaseModel):
+	a: int
+	b: int	
+
+@app.post("/adder/")
+def adder( ops : Operand): #Depend on the class Operad variables. It's like create a new type
+	return({"return": ops.a + ops.b })
+	
